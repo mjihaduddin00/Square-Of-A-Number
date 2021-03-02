a3777b37970492d55ba6e919d896a8d80425c1d1
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-01 23:07:19 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,14 @@
+/* 
+Square of a number
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(sqr(5));
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
